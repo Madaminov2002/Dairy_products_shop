4524657470d9a6807e57e069589faa7132ba9474
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-06-03 10:19:18 +0500

    first commit

---
+++ src/main/java/org/example/dairy_products_shop/DairyProductsShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.dairy_products_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyProductsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DairyProductsShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dairy_products_shop/DairyProductsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dairy_products_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyProductsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
